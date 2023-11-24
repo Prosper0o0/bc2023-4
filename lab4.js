0b43fae2d70332bac49e188cd322e8e3fff73154
Prosper0o0 <andsp15726@gmail.com>
2023-11-24 15:39:44 +0200

    12412

--- lab4.js
+++ lab4.js
@@ -11,41 +11,42 @@ const options = {
 };
 const app = express();
 const port = 8000;
-fs.readFile("data.xml", "utf8", (err, data) => {
-    if (err) {
-        console.error('Помилка при читанні файлу:', err);
-        return;
-    }
-    const parser = new XMLParser(options);
-    const obj = parser.parse(data);
-    let maxRate = -Infinity;
-    let currencies;
 
-    if (Array.isArray(obj.exchange.currency)) {
-        currencies = obj.exchange.currency;
-    } else {
-        currencies = [obj.exchange.currency];
-    }
-    for (const currency of currencies) {
-        if (currency.rate) {
-            const rate = parseFloat(currency.rate);
-            if (!isNaN(rate) && rate > maxRate) {
-                maxRate = rate;
-            }
-        }
-    }
-    const resultXml = xmlbuilder.create('data')
-        .ele('max_rate', maxRate.toString())
-        .end({ pretty: true });
-    fs.writeFile("result.xml", resultXml.toString(), (err) => {
+app.get('/', (req, res) => {
+    fs.readFile("data.xml", "utf8", (err, data) => {
         if (err) {
-            console.error('Помилка при записі у файл:', err);
+            console.error('Помилка при читанні файлу:', err);
             return;
         }
-        console.log("Yeah!");
+        const parser = new XMLParser(options);
+        const obj = parser.parse(data);
+        let maxRate = -Infinity;
+        let currencies;
+
+        if (Array.isArray(obj.exchange.currency)) {
+            currencies = obj.exchange.currency;
+        } else {
+            currencies = [obj.exchange.currency];
+        }
+        for (const currency of currencies) {
+            if (currency.rate) {
+                const rate = parseFloat(currency.rate);
+                if (!isNaN(rate) && rate > maxRate) {
+                    maxRate = rate;
+                }
+            }
+        }
+        const resultXml = xmlbuilder.create('data')
+            .ele('max_rate', maxRate.toString())
+            .end({ pretty: true });
+        fs.writeFile("result.xml", resultXml.toString(), (err) => {
+            if (err) {
+                console.error('Помилка при записі у файл:', err);
+                return;
+            }
+            console.log("Yeah!");
+        });
     });
-});
-app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'application/xml');
     res.sendFile(__dirname + '/result.xml');
 });

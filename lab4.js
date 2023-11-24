b683147dae1419bd454f958f62d1a5d7c265b39c
Prosper0o0 <andsp15726@gmail.com>
2023-11-24 15:24:52 +0200

    124

--- lab4.js
+++ lab4.js
@@ -9,33 +9,29 @@ const options = {
     textNodeName: '_',
     ignoreAttributes: false,
 };
-
 const app = express();
 const port = 8000;
-
 fs.readFile("data.xml", "utf8", (err, data) => {
     if (err) {
         console.error('Помилка при читанні файлу:', err);
         return;
     }
-
     const parser = new XMLParser(options);
     const obj = parser.parse(data);
     let maxRate = -Infinity;
 
     for (const currency of obj.exchange.currency) {
-        const rate = parseFloat(currency.rate);
-        if (rate > maxRate) {
-            maxRate = rate;
+        if (currency.rate) {
+            const rate = parseFloat(currency.rate);
+            if (!isNaN(rate) && rate > maxRate) {
+                maxRate = rate;
+            }
         }
     }
-
     console.log(maxRate);
-
     const resultXml = xmlbuilder.create('data')
         .ele('max_rate', maxRate.toString())
         .end({ pretty: true });
-
     fs.writeFile("result.xml", resultXml.toString(), (err) => {
         if (err) {
             console.error('Помилка при записі у файл:', err);
@@ -48,7 +44,6 @@ app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'application/xml');
     res.sendFile(__dirname + '/result.xml');
 });
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
